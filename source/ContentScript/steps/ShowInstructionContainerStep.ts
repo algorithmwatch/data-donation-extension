41585e7d987d95c9c3eb7673d9763feab916f8b8
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2021-08-24 03:09:00 +0200

    wip

--- source/ContentScript/steps/ShowInstructionContainerStep.ts
+++ source/ContentScript/steps/ShowInstructionContainerStep.ts
@@ -2,6 +2,8 @@ import {render} from 'react-dom';
 import Step from '../Step';
 import Container from './Container';
 
+// TODO: https://itnext.io/create-chrome-extension-with-reactjs-using-inject-page-strategy-137650de1f39
+
 class ShowInstructionContainerStep extends Step {
   async run(): Promise<void> {
     render(Container(this.props), document.querySelector(this.props.target));
